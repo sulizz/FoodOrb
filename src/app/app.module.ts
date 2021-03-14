3aebacee747735785a602d28dde5b0f60925aeb6
sulizbasnet <sulizzbasnet@gmail.com>
2021-03-14 16:26:14 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {RegistrationModule} from './registration/registration.module';
+import { IndexComponent } from './index/index.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RegistrationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
