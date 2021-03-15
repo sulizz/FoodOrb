0560c3b5a41682a2da4c00d9f76a14529972e9d8
sulizbasnet <sulizzbasnet@gmail.com>
2021-03-15 00:48:17 -0700

    forgot password template working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { ForgetpasswordComponent } from './registration/forgetpassword/forgetpassword.component';
 import { LoginComponent } from './registration/login/login.component';
 import { RegisterComponent } from './registration/register/register.component';
 
 const routes: Routes = [
-    {path:'register',component:RegisterComponent},
-    {path:'login',component:LoginComponent},
-    {path:'index',component:IndexComponent},
-    {path: '', redirectTo: 'index', pathMatch: 'full'}
+    {
+        path:'register',
+        component:RegisterComponent
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'index',
+        component:IndexComponent
+    },
+    {
+        path: '', 
+        redirectTo: 'index', 
+        pathMatch: 'full'
+    },
+    {
+        path:'password/reset',
+        component:ForgetpasswordComponent
+    }
+
 ];
 
 @NgModule({
--- src/app/registration/login/login.component.ts
+++ src/app/registration/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { MustMatch } from '../must-match.validator';
 
 
 @Component({
